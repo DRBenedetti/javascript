37ee428bc577e09583a350a68ff541735ce6bea5
DRBenedetti <144566798+DRBenedetti@users.noreply.github.com>
2024-08-18 20:41:02 -0300

    Crie mais um exercicio

---
+++ ex011/ex011.js
@@ -0,0 +1,12 @@
+var idade = 22
+if (idade >= 16) {
+    if (idade >= 16 && idade < 18) {
+        console.log('Voto opcional')
+    }
+    else {
+        console.log('Vota')
+    }
+}
+else {
+    console.log('Não vota')
+}
