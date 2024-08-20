f8e6854c6a3b6a0926e93027c8d664706b77a2d1
DRBenedetti <144566798+DRBenedetti@users.noreply.github.com>
2024-08-19 21:34:27 -0300

    Melhorei o ex011

--- ex011/ex011.js
+++ ex011/ex011.js
@@ -1,12 +1,17 @@
-var idade = 22
-if (idade >= 16) {
-    if (idade >= 16 && idade < 18) {
-        console.log('Voto opcional')
-    }
-    else {
-        console.log('Vota')
-    }
+var idade = 50
+if (idade < 16) {
+    console.log('Não vota!')
+
+} 
+else if (idade < 18 || idade >= 65) {
+    console.log('Voto Opcional!')
 }
 else {
-    console.log('Não vota')
+    console.log('Voto obrigatorio!')
 }
+
+/*
+&& == or
+
+|| == and
+*/
