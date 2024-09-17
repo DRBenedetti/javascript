3b0f0382f37ec983fdea9c83e35286ee2fce9291
DRBenedetti <144566798+DRBenedetti@users.noreply.github.com>
2024-09-17 13:51:53 -0300

    Treinando Js

---
+++ aula013/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+/*
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+*/
